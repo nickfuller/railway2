1724080528619cf3f6c108b33a827eff3fe651ce
James Thomason <vandebar@gmail.com>
2012-08-23 16:04:16 -0500

    up to naming routes according to conventions

--- app/controllers/lines_controller.rb
+++ app/controllers/lines_controller.rb
@@ -1,21 +1,51 @@
 class LinesController < ApplicationController
 
-def index
-	@line = TrainLine.all
-end
+	def index
+		@lines = TrainLine.all
+	end
 
-def new
-end
+	def new
+	end
 
-def create
-	@line = TrainLine.new
-	@line.name = params[:name]
-	@line.frequency = params[:frequency]
-	@line.save
-	redirect_to 'http://localhost:3000/lines'
-end
+	def create
+		@line = TrainLine.new
+		@line.name = params[:name]
+		@line.frequency = params[:frequency]
+		# would params[frequency] <= no colon work?
+		@line.save
+		redirect_to 'http://localhost:3000/lines'
+	end
 
-def show
-end
+	def show
+		line_id = params[:id]
+		@line = TrainLine.find_by_id(line_id)
+	end
 
+	def edit
+		line_id = params[:id]
+		@line = TrainLine.find_by_id(line_id)
+	end
+
+	def update	
+		# NOT understanding everything happening here in the update action.
+		line_id = params[:id]
+		name = params[:name]
+		frequency = params[:frequency]
+	
+		@line = TrainLine.find_by_id(line_id)
+	
+		@line.name = name
+		@line.frequency = frequency
+		@line.save
+	
+		redirect_to 'http://localhost:3000/lines/#{@line.id}'
+	end
+	
+	def destroy
+		line_id = params[:id]
+		@line = TrainLine.find_by_id(line_id)
+		@line.destroy
+		redirect_to 'http://localhost:3000/lines'
+	end
+	
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Railway2::Application.routes.draw do
   
-  get 'lines' => 'Lines#index'
-  get 'lines/new' => 'Lines#new'
-  post 'lines' => 'Lines#create'
-  get 'lines/:id' => 'Lines#show'
+  get 'lines' => 'Lines#index', :as => 'train_lines'
+  get 'lines/new' => 'Lines#new', :as => 'new_train_line'
+  post 'lines' => 'Lines#create', :as => 'train_lines'
+	# WILL this conflict with index?
+  get 'lines/:id' => 'Lines#show', :as => 'train_line'
+	get 'lines/:id/edit' => 'Lines#edit', :as => 'edit_train_line'
+	put 'lines/:id' => 'Lines#update'
+	delete 'lines/:id' => 'Lines#destroy', :as => 'train_line'
   
 end
